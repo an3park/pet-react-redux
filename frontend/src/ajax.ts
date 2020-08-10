6ad6bc87f719f636cafc25d01e0bfac5c9aa77b0
Andrey <andrepolugodin@gmail.com>
2020-08-10 23:13:32 +0300

    analyze

--- frontend/src/ajax.ts
+++ frontend/src/ajax.ts
@@ -1,7 +1,7 @@
 import ky from 'ky'
 import IUser from '@interfaces/User'
 
-const BASE_URL = ''
+const BASE_URL = 'http://127.0.0.1:3000'
 
 export async function fetchUsers() {
   const users: IUser[] = await ky.get(BASE_URL + '/api/user').json()
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,17 +2,25 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = (env = {}) => {
-  const isDev = !!env.dev
+  const isProd = !env.dev
+  const isDev = !isProd
   isDev && console.log('=== DEV MODE ===')
 
   const filename = ext => (isDev ? `[name].${ext}` : `[hash].${ext}`)
 
-  const cssLoaders = [
-    'style-loader',
-    'css-loader'
+  const cssLoaders = ['style-loader', 'css-loader']
+  const plugins = [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin()
   ]
+  if (isProd) {
+    plugins.push(new BundleAnalyzerPlugin())
+  }
 
   return {
     context: path.resolve(__dirname, 'src'),
@@ -41,12 +49,7 @@ module.exports = (env = {}) => {
           minimize: true,
           minimizer: [new TerserPlugin()]
         },
-    plugins: [
-      new HTMLWebpackPlugin({
-        template: './index.html'
-      }),
-      new CleanWebpackPlugin()
-    ],
+    plugins,
     module: {
       rules: [
         {
@@ -68,10 +71,7 @@ module.exports = (env = {}) => {
             {
               loader: 'babel-loader',
               options: {
-                presets: [
-                  '@babel/preset-env',
-                  '@babel/preset-react'
-                ],
+                presets: ['@babel/preset-env', '@babel/preset-react'],
                 plugins: ['@babel/plugin-transform-runtime']
               }
             }
